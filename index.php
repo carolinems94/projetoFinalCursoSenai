fade0207b4777aacb76cff4f38f996f2bd689fd3
carolinems94 <carolinems94@hotmail.com>
2024-03-02 14:09:33 -0300

    criação header e footer index

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compra de Carro</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="css/styles.css">
+
+
+
+</head>
+
+<body>
+
+
+
+    <header>
+        <div class="content">
+            <nav>
+                <div class="topo-site">
+                    <a class="titulo" href="index.php">AudiCar</a>
+                </div>
+                <ul>
+                    <li><a class="nav-link" href="index.php"></a></li>
+                    <li><a class="nav-link" href="?page=catalogo">Catálogo Completo</a></li>
+                    <li><a class="nav-link" href="?page=vercarros">Editar catálogo</a></li>
+                    <li><a class="nav-link" href="?page=cadastro">Cadastrar novo veiculo</a></li>
+
+                    <button>Login</button>
+                </ul>
+            </nav>
+            <div class="header-block">
+                <div class="text">
+                    <h2>O carro perfeito para você</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+                        asperiores?
+                    </p>
+                </div>
+                <img src="imagens/car-header.png" alt="Car" />
+            </div>
+        </div>
+    </header>
+</body>
+
+<footer>
+    <div class="main">
+        <div class="content footer-links">
+            <div class="footer-social">
+                <h4>Redes Sociais</h4>
+                <div class="social-icons">
+                    <img src="imagens/instagram.png" alt="Instagram" />
+                    <img src="imagens/facebook.png" alt="Facebook" />
+                </div>
+            </div>
+            <div class="footer-contact">
+                <h4>Contatos</h4>
+                <h6>+55 71 999999999</h6>
+                <h6>audicar@senai.com.br</h6>
+                <h6>Nome da Rua, Salvador BA</h6>
+            </div>
+        </div>
+    </div>
+    <!-- <div class="last"><a class="last nav-link" href="?page=politicacookies">CarolCar 2024 - Política de Cookies</a>
+    </div> -->
+</footer>
+
+
+</html>
